1a2dcd62d894ba2e57e107a81e8e6143a8f43f2b
Marco Valerio <mo.valerio93@gmal.com>
2022-10-10 23:54:02 +0200

    create done

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,44 @@
+@extends('layout.app')
+
+@section('title', 'Crea nuovo fumetto')
+
+@section('content')
+<div class="container">
+    <form action="{{route('comics.store')}}"methods="POST">
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Title</label>
+          <input type="text" class="form-control" id="title" name="tilte"/>
+        </div>  
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">description</label>
+            <textarea id="description" name="description" class="from-control"></textarea> 
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">thumb</label>
+            <input type="text" class="form-control" id="thumb" name="thumb"/>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">price</label>
+            <input type="text" class="form-control" id="price" name="price"/>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">series</label>
+            <input type="text" class="form-control" id="series" name="series"/>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">sale date</label>
+            <input type="text" class="form-control" id="sale_date" name="sale_date"/>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">type</label>
+            <input type="text" class="form-control" id="type" name="type"/>
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Salva</button>
+    </form>
+
+</div>
+
+
+@endsection
+
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,17 +5,19 @@
 @section('content')
 
     <div class="container">
+
+        <a class="btn btn-primary" href="{{route('comics.create')}}">Crea</a>
+
         <table class="table">
             <thead>
             <tr>
                 <th scope="col">#</th>
                 <th scope="col">title</th>
-                <th scope="col">description</th>
-                <th scope="col">thumb</th>
                 <th scope="col">price</th>
                 <th scope="col">series</th>
                 <th scope="col">sale_data</th>
-                <th scope="col">type</th>
+                <th scope="col">tipe</th>
+                <th scope="col">action</th>
             </tr>
             </thead>
             <tbody>
@@ -23,15 +25,14 @@
                     <tr>
                         <th scope="row">{{$fumetto->id}}</th>
                         <td>{{$fumetto->title}}</td>
-                        <td>{{$fumetto->description}}</td>
-                        <td>{{$fumetto->thumb}}</td>
                         <td>{{$fumetto->price}}</td>
                         <td>{{$fumetto->series}}</td>
                         <td>{{$fumetto->sale_date}}</td>
-                        <td>{{$fumetto->type}}</td>                     
+                        <td>{{$fumetto->type}}</td>   
+                        <td><a class="btn btn-primary" href="{{route('comics.show', ['comic'=> $fumetto->id])}}">Vedi</a></td>                  
                     </tr> 
                  @endforeach
-            </tbody>
+            </tbody>  
         </table>
     </div>
 
